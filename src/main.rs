9e463fe6e6ee617897f88807a00da57f5befa2a6
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2020-05-16 23:14:12 +0900

    四角を表示する

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
 extern crate termion;
 
-use termion::{color, style};
+use termion::event::{Event, Key};
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use termion::{clear, cursor};
 
-// use std::io;
+use std::io::{stdin, stdout, Write};
 
 fn main() {
-  println!("{}Red", color::Fg(color::Red));
-  println!("{}Blue", color::Fg(color::Blue));
-  println!("{}Blue'n'Bold{}", style::Bold, style::Reset);
-  println!("{}Just plain italic", style::Italic);
+  // Get and lock the stdios.
+  let stdout = stdout();
+  // We go to raw mode to make the control over the terminal more fine-grained.
+  let mut stdout = stdout.into_raw_mode().unwrap();
+
+  write!(stdout, "{}", clear::All).unwrap();
+  write!(stdout, "{}", cursor::Goto(1, 1)).unwrap();
+
+  write!(stdout, "■").unwrap();
+  write!(stdout, "□").unwrap();
+  write!(stdout, "■").unwrap();
+  write!(stdout, "□").unwrap();
+
+  stdout.flush().unwrap();
+
+  let stdin = stdin();
+
+  for evt in stdin.events() {
+    let e = evt.unwrap();
+    match e {
+      Event::Key(Key::Char('q')) => break,
+      _ => {}
+    }
+  }
 }
